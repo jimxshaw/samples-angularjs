d3adca2d869716d7366d66d257164104e77686ee
JXS <jimxshaw@gmail.com>
2015-12-13 19:37:02 -0600

    added properties to controller

--- sample-app/app/book/booksController.js
+++ sample-app/app/book/booksController.js
@@ -9,6 +9,16 @@
     // if the controller has a property which does not utilize the scope then 
     // it can be accessed from the view by using controller as syntax.
     this.greeting = "This is a greeting message using controller as syntax";
+
+    $scope.books = [];
+    $scope.fetchBooks = function() {
+      $scope.books = [
+        {ID: 1, Title: "The Hobbit", Author: "JRR Tolkein", ISBN: "BEB37B382"},
+        {ID: 2, Title: "Dune", Author: "Frank Herbert", ISBN: "467BKLJ3MN"},
+        {ID: 3, Title: "The Catcher in the Rye", Author: "JD Salinger", ISBN: "LKD47382"},
+        {ID: 4, Title: "Misery", Author: "Stephen King", ISBN: "J7N37HE"}
+      ];
+    };
   }
   angular.module('myAngularApplication').controller('booksController', ["$scope", booksController]); 
 }());
