bbcd6a9a2b9d116c79cedef8c0057fb135bc9b3f
Jim Shaw <jimxshaw@gmail.com>
2015-12-27 18:06:32 -0600

    used ng-repeat to display event sessions info

--- EventsRegistry/app/js/controllers/EventController.js
+++ EventsRegistry/app/js/controllers/EventController.js
@@ -10,8 +10,32 @@ eventsApp.controller('EventController',
                 address: 'Google HQ',
                 city: 'Mountain View',
                 province: 'CA'
-            }
+            },
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Mastering Directives',
+                    creatorName: 'Tyrion Lannister',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'Fusce scelerisque lorem pretium fringilla maximus.'
+                },
+                {
+                    name: 'What is Scope?',
+                    creatorName: 'Jorah Mornmont',
+                    duration: '.5 hr',
+                    level: 'Novice',
+                    abstract: 'Morbi ac enim sit amet lacus lobortis ultrices.'
+                },
+                {
+                    name: 'All about Controllers',
+                    creatorName: 'Samwell Tarly',
+                    duration: '1 hr',
+                    level: 'Intermediate',
+                    abstract: 'Aenean venenatis luctus massa, ut molestie turpis luctus eget.'
+                }
+            ]
         };
     }
-);
+    );
 
