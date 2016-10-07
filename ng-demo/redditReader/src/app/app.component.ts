382be237e24cf5b6cc8882ae4804c96d29adc19d
Jim Shaw <jimxshaw@gmail.com>
2016-10-07 10:25:50 -0500

    Implemented AppComponent of app.component.ts.

--- ng-demo/redditReader/src/app/app.component.ts
+++ ng-demo/redditReader/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { Headers, Jsonp, Response } from '@angular/http';
+import { ApiResponse, Submission } from './reddit';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  
+  private feedData: Observable<Submission[]>;
+
+  constructor(private jsonp: Jsonp) {
+
+  }
+
+  ngOnInit() {
+    this.displaySubreddit();
+  }
+
+  private displaySubreddit() {
+    this.feedData = this.jsonp.get('http://reddit.com/r/aww.json?jsonp=JSONP_CALLBACK')
+    .map(res => (res.json() as ApiResponse).data.children
+      .filter(({data}) => /(png|jpg)$/.test(data.url)).map(child => child.data));
+  }
 }
