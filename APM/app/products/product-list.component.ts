8d1d0742f7f3fd5caf8cba6cb9090d1b661e36a1
Jim Shaw <jimxshaw@gmail.com>
2016-11-02 23:02:56 -0500

    In product list component, subscribed to our products list returned from the product service.

--- APM/app/products/product-list.component.ts
+++ APM/app/products/product-list.component.ts
@@ -21,6 +21,8 @@ export class ProductListComponent implements OnInit {
 
     listFilter: string;
 
+    errorMessage: string;
+
     products: IProduct[];
 
     constructor(private _productService: ProductService) {
@@ -32,7 +34,9 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this._productService.getProducts();
+        this._productService.getProducts()
+            .subscribe(products => this.products = products,
+                        error => this.errorMessage = <any>error);
     }
 
     onRatingClicked(message: string): void {
