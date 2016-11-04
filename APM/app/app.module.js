e990331b78e25e27452063535748f63f82fd0a63
Jim Shaw <jimxshaw@gmail.com>
2016-11-03 22:21:02 -0500

    Imported RouterModule and registered it in app module. Defined some routes.

--- APM/app/app.module.js
+++ APM/app/app.module.js
@@ -18,6 +18,7 @@ var product_filter_pipe_1 = require("./products/product-filter.pipe");
 var star_component_1 = require("./shared/star.component");
 var welcome_component_1 = require("./home/welcome.component");
 var product_detail_component_1 = require("./products/product-detail.component");
+var router_1 = require("@angular/router");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -26,7 +27,14 @@ var AppModule = (function () {
             imports: [
                 platform_browser_1.BrowserModule,
                 forms_1.FormsModule,
-                http_1.HttpModule
+                http_1.HttpModule,
+                router_1.RouterModule.forRoot([
+                    { path: "products", component: product_list_component_1.ProductListComponent },
+                    { path: "product/:id", component: product_detail_component_1.ProductDetailComponent },
+                    { path: "welcome", component: welcome_component_1.WelcomeComponent },
+                    { path: "", redirectTo: "welcome", pathMatch: "full" },
+                    { path: "**", redirectTo: "welcome", pathMatch: "full" }
+                ])
             ],
             declarations: [
                 app_component_1.AppComponent,
--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -9,12 +9,20 @@ import { ProductFilterPipe } from "./products/product-filter.pipe";
 import { StarComponent } from "./shared/star.component";
 import { WelcomeComponent } from "./home/welcome.component";
 import { ProductDetailComponent } from "./products/product-detail.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   imports: [
       BrowserModule,
       FormsModule,
-      HttpModule
+      HttpModule,
+      RouterModule.forRoot([
+          { path: "products", component: ProductListComponent },
+          { path: "product/:id", component: ProductDetailComponent },
+          { path: "welcome", component: WelcomeComponent },
+          { path: "", redirectTo: "welcome", pathMatch: "full" },
+          { path: "**", redirectTo: "welcome", pathMatch: "full" }
+      ])
   ],
   declarations: [
       AppComponent,
