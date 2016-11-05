e67020a9c7b546b64a76c01151bb78f553df912e
Jim Shaw <jimxshaw@gmail.com>
2016-11-04 20:44:32 -0500

    Hooked up routing using product id inside product list template leading to product detail template.

--- APM/app/products/product-detail.component.js
+++ APM/app/products/product-detail.component.js
@@ -9,15 +9,21 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
 var ProductDetailComponent = (function () {
-    function ProductDetailComponent() {
+    function ProductDetailComponent(_route) {
+        this._route = _route;
         this.pageTitle = "Product Detail";
     }
+    ProductDetailComponent.prototype.ngOnInit = function () {
+        var id = +this._route.snapshot.params["id"];
+        this.pageTitle += ": " + id;
+    };
     ProductDetailComponent = __decorate([
         core_1.Component({
             templateUrl: "app/products/product-detail.component.html"
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [router_1.ActivatedRoute])
     ], ProductDetailComponent);
     return ProductDetailComponent;
 }());
