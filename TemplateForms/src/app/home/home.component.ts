dd7fb3e5c9c66ad62a85b5aa88540ead6cf87a44
Jim Shaw <jimxshaw@gmail.com>
2016-11-06 22:37:05 -0600

    Tested data binding with various form controls.

--- TemplateForms/src/app/home/home.component.ts
+++ TemplateForms/src/app/home/home.component.ts
@@ -13,16 +13,27 @@ export class HomeComponent {
     "Other"
   ];
 
-  model = new Employee("George", "Washington");
+  model = new Employee("George", "Washington", true, "W2", "English", new Date("October 31, 2016").toLocaleDateString());
 
-  firstNameToUpperCase(value: string) {
+  firstNameToUpperCase(value: string, isFirstName: boolean) {
+
     if (value.length > 0) {
-      this.model.firstName = value.charAt(0).toLocaleUpperCase() + value.slice(1);
+      if (isFirstName) {
+        this.model.firstName = value.charAt(0).toLocaleUpperCase() + value.slice(1);
+      }
+      else {
+        this.model.lastName = value.charAt(0).toLocaleUpperCase() + value.slice(1);
+      }
     }
     else {
-      this.model.firstName = value;
+      if (isFirstName) {
+        this.model.firstName = value;
+      }
+      else {
+        this.model.lastName = value;
+      }
     }
   }
 
-  
+
 }
--- TemplateForms/src/app/models/Employee.model.ts
+++ TemplateForms/src/app/models/Employee.model.ts
@@ -1,11 +1,18 @@
 export class Employee {
 
   firstName: string;
-
   lastName: string;
+  isFullTime: boolean;
+  paymentType: string;
+  primaryLanguage: string;
+  dateHired: string;
 
-  constructor(firstName: string, lastName: string) {
+  constructor(firstName: string, lastName: string, isFullTime: boolean, paymentType: string, primaryLanguage: string, dateHired: string) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.isFullTime = isFullTime;
+    this.paymentType = paymentType;
+    this.primaryLanguage = primaryLanguage;
+    this.dateHired = dateHired;
   }
 }
