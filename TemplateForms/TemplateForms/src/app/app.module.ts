22a3c4e7123655123eb3c1cb9c0b2650998bb83f
Jim Shaw <jimxshaw@gmail.com>
2016-12-06 21:50:43 -0600

    Tested ng2-bootstrap's DatePicker.

--- TemplateForms/TemplateForms/src/app/app.module.ts
+++ TemplateForms/TemplateForms/src/app/app.module.ts
@@ -13,10 +13,11 @@ import {RepoListComponent} from './github/repo-list/repo-list.component';
 import {RepoDetailComponent} from './github/repo-detail/repo-detail.component';
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {FormPoster} from "./services/form-poster.service";
+import { DatepickerModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, AboutComponent, RepoBrowserComponent, RepoListComponent, RepoDetailComponent, HomeComponent],
-  imports     : [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(rootRouterConfig)],
+  imports     : [DatepickerModule, BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(rootRouterConfig)],
   providers   : [FormPoster, GithubService, {provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap   : [AppComponent]
 })
--- TemplateForms/TemplateForms/src/app/home/home.component.ts
+++ TemplateForms/TemplateForms/src/app/home/home.component.ts
@@ -1,67 +1,85 @@
-import { Component } from '@angular/core';
-import { Employee } from "../models/Employee.model";
-import { FormPoster } from "../services/form-poster.service";
-import { NgForm } from "@angular/forms";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'home',
-  styleUrls: ['./home.component.css'],
-  templateUrl: './home.component.html'
+  selector: "home",
+  styleUrls: ["./home.component.css"],
+  templateUrl: "./home.component.html"
 })
 export class HomeComponent {
-  languages = [];
 
-  // model = new Employee("George", "Washington", true, "W2", "English", new Date("October 31, 2016").toLocaleDateString());
+  startDate: Date;
+  minDate = new Date("Jan 1 2017");
 
-  model = new Employee("", "", false, "", "default", "");
+  constructor() {
 
-  hasPrimaryLanguageError = false;
-
-  constructor(private formPoster: FormPoster) {
-    this.formPoster.getLanguages()
-                    .subscribe(data => this.languages = data.languages, error => console.log("get error: ", error));
   }
-
-  submitForm(form: NgForm) {
-    // Validate form.
-    this.validatePrimaryLanguage(this.model.primaryLanguage);
-
-    if (this.hasPrimaryLanguageError) {
-      return;
-    }
-
-    this.formPoster.postEmployeeForm(this.model)
-                      .subscribe(data => console.log("success: ", data), error => console.log("error: ", error));
-  }
-
-  validatePrimaryLanguage(value) {
-    if (value === "default") {
-      this.hasPrimaryLanguageError = true;
-    }
-    else {
-      this.hasPrimaryLanguageError = false;
-    }
-  }
-
-  firstNameToUpperCase(value: string, isFirstName: boolean) {
-
-    if (value.length > 0) {
-      if (isFirstName) {
-        this.model.firstName = value.charAt(0).toLocaleUpperCase() + value.slice(1);
-      }
-      else {
-        this.model.lastName = value.charAt(0).toLocaleUpperCase() + value.slice(1);
-      }
-    }
-    else {
-      if (isFirstName) {
-        this.model.firstName = value;
-      }
-      else {
-        this.model.lastName = value;
-      }
-    }
-  }
-
 
 }
+
+// import { Component } from '@angular/core';
+// import { Employee } from "../models/Employee.model";
+// import { FormPoster } from "../services/form-poster.service";
+// import { NgForm } from "@angular/forms";
+//
+// @Component({
+//   selector: 'home',
+//   styleUrls: ['./home.component.css'],
+//   templateUrl: './home.component.html'
+// })
+// export class HomeComponent {
+//   languages = [];
+//
+//   // model = new Employee("George", "Washington", true, "W2", "English", new Date("October 31, 2016").toLocaleDateString());
+//
+//   model = new Employee("", "", false, "", "default", "");
+//
+//   hasPrimaryLanguageError = false;
+//
+//   constructor(private formPoster: FormPoster) {
+//     this.formPoster.getLanguages()
+//                     .subscribe(data => this.languages = data.languages, error => console.log("get error: ", error));
+//   }
+//
+//   submitForm(form: NgForm) {
+//     // Validate form.
+//     this.validatePrimaryLanguage(this.model.primaryLanguage);
+//
+//     if (this.hasPrimaryLanguageError) {
+//       return;
+//     }
+//
+//     this.formPoster.postEmployeeForm(this.model)
+//                       .subscribe(data => console.log("success: ", data), error => console.log("error: ", error));
+//   }
+//
+//   validatePrimaryLanguage(value) {
+//     if (value === "default") {
+//       this.hasPrimaryLanguageError = true;
+//     }
+//     else {
+//       this.hasPrimaryLanguageError = false;
+//     }
+//   }
+//
+//   firstNameToUpperCase(value: string, isFirstName: boolean) {
+//
+//     if (value.length > 0) {
+//       if (isFirstName) {
+//         this.model.firstName = value.charAt(0).toLocaleUpperCase() + value.slice(1);
+//       }
+//       else {
+//         this.model.lastName = value.charAt(0).toLocaleUpperCase() + value.slice(1);
+//       }
+//     }
+//     else {
+//       if (isFirstName) {
+//         this.model.firstName = value;
+//       }
+//       else {
+//         this.model.lastName = value;
+//       }
+//     }
+//   }
+//
+//
+// }
