eaa13333f03961806eadc2fe3034ddcf3f39b28e
Jim Shaw <jimxshaw@gmail.com>
2017-06-28 11:22:06 -0500

    added github viewer project boilerplate.

---
+++ githubViewer/script.js
@@ -0,0 +1,36 @@
+(function () {
+    'use strict';
+
+    var app = angular.module('githubViewer', [
+        // Angular modules
+
+        // Custom modules
+
+        // 3rd Party Modules
+
+    ]);
+
+    var MainController = function ($scope, $http) {
+
+    };
+
+    // For minification purposes, we pass in $ variables into the array so Angular
+    // will be aware of them when it replaces them with minifided version. The last 
+    // item in the array is the registered controller.
+    app.controller("MainController", ["$scope", "$http", MainController]);
+
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
